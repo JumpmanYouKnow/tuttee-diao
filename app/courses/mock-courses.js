cf883e8820289ca89a12e15971d28299aacc3226
zzq229 <zhouziqi960620@gmail.com>
2016-03-22 19:34:06 -0400

    add images to preview

--- app/courses/mock-courses.js
+++ app/courses/mock-courses.js
@@ -9,14 +9,14 @@ System.register([], function(exports_1, context_1) {
                 {
                     id: "math135",
                     name: "Math 135",
-                    //imageURL: "http://placehold.it/500x500",
+                    imageURL: "http://placehold.it/500x500",
                     description: "Math's 135's Descriptions",
                     total_tutors: 5,
                 },
                 {
                     id: "math137",
                     name: "Math 137",
-                    ///imageURL: "http://placehold.it/500x300",
+                    imageURL: "http://placehold.it/500x500",
                     description: "Math's 137's Descriptions",
                     total_tutors: 3888,
                 },
