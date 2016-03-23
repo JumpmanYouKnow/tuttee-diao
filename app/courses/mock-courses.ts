f889ff41f9182631dfd77effe221fe5e23259dbe
zzq229 <zhouziqi960620@gmail.com>
2016-03-22 20:17:28 -0400

    add image for courses part

--- app/courses/mock-courses.ts
+++ app/courses/mock-courses.ts
@@ -7,14 +7,15 @@ export var COURSES: Course[] = [
 		imageURL: "http://placehold.it/500x500",
 		description: "Math's 135's Descriptions",
 		total_tutors: 5,
-		//sales:5
+		sales:5
+
 	},
     {
 		id: "math137",
 		name: "Math 137",
 		imageURL: "http://placehold.it/500x500",
 		description: "Math's 137's Descriptions",
-		total_tutors: 3888,
-		//sales: 9999
+		total_tutors: 3,
+		sales: 9
 	},
 ];
